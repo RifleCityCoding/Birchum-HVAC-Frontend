f05f6a287a18ff947351ebaaa661f3cb9f4ccb28
RifleCityCoding <142933516+RifleCityCoding@users.noreply.github.com>
2024-01-18 00:31:22 -0500

    More vercel changes

--- fastapi-frontend/src/pages/Account/index.js
+++ fastapi-frontend/src/pages/Account/index.js
@@ -37,7 +37,7 @@ export default function Home() {
       }
     };
     getUserFromLocalStorage();
-  }, []);
+  }, [dispatch]);
 
   const handleLogout = () => {
     authService.logout();
--- fastapi-frontend/src/pages/Calendar/index.js
+++ fastapi-frontend/src/pages/Calendar/index.js
@@ -32,7 +32,7 @@ export default function Home() {
       }
     };
     getUserFromLocalStorage();
-  }, []);
+  }, [dispatch]);
 
   const handleLogout = () => {
     authService.logout();
